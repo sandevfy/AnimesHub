e5683e845082d79b3d709fbd4f42727c7df34059
sandevfy <dsan.watchx@gmail.com>
2026-05-23 21:43:23 -0300

    Model UsuarioAnime
    Criando a classe model de UsuarioAnime, relacionamento entre Usuario e Anime

--- Models/UsuarioAnime.cs
+++ Models/UsuarioAnime.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AnimesHub.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,12 @@ namespace AnimesHub.Models
 {
     internal class UsuarioAnime
     {
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+
+        public int AnimeId { get; set; }
+        public Anime Anime { get; set; }
+
+        public ModeNivel ModeNivel { get; set; }
     }
 }
