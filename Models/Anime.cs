ed2ff6399e873724938c478f20ca5b608ac8abdb
sandevfy <dsan.watchx@gmail.com>
2026-05-23 21:41:27 -0300

    Model Anime
    Criando a classe model de Anime

--- Models/Anime.cs
+++ Models/Anime.cs
@@ -6,5 +6,12 @@ namespace AnimesHub.Models
 {
     internal class Anime
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Ano { get; set; }
+        public int Temporadas { get; set; }
+        public int Episodios { get; set; }
+        public string Descrition { get; set; }
+        public List<UsuarioAnime> UsuarioAnimes { get; set; }
     }
 }
